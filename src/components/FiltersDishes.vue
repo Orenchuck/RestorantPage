5233219d155c47ccb1f938acceb71e06e69ac425
orenchuck <golub.ptica@gmail.com>
2022-04-22 15:07:08 +0300

    svg as a component

--- src/components/FiltersDishes.vue
+++ src/components/FiltersDishes.vue
@@ -3,31 +3,9 @@
     :class="show_menu_mob"
     class="filter_box">
     <div class="page_logo">
-        <!-- <p> O </p> -->
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 107.941 48.489">
-      <g data-name="Group 1409">
-        <path data-name="Path 1087" 
-        d="M12.02 24.449a12.02 12.02 0 1 0 12.02 12.02 12.02 12.02 0 0 0-12.02-12.02zm0 21.093a9.074 9.074 0 1 1 9.074-9.074 9.074 9.074 0 0 1-9.074 9.074z"
-         fill="#1f1917">
-         </path>
-         <path data-name="Path 1088" 
-         d="M40.301 24.449a12.02 12.02 0 1 0 12.02 12.02 12.02 12.02 0 0 0-12.02-12.02zm0 21.093a9.074 9.074 0 1 1 9.074-9.074 9.074 9.074 0 0 1-9.074 9.074z" 
-         fill="#1f1917">
-         </path>
-         <path data-name="Rectangle 1572" 
-         fill="#1f1917" d="M81.795 26.514l2.225-2.225 21.931 21.93-2.225 2.226z">
-         </path>
-         <path data-name="Rectangle 1573" fill="#1f1917" d="M69.643 24.331h5.774v24.157h-5.774z"></path>
-         <path data-name="Rectangle 1574" fill="#1f1917" d="M56.916.057h5.774v48.432h-5.774z"></path>
-         <path data-name="Rectangle 1575" fill="#1f1917" d="M84.019 48.445l-2.225-2.225 21.93-21.931 2.226 2.225z"></path>
-         <path data-name="Line 211" fill="none" d="M71.293 24.275h36.648"></path>
-         <path data-name="Line 212" fill="none" d="M59.391 0H23.332"></path>
-         <path data-name="Path 1089" d="M34.055 29.824V0h-5.774v36.295h2.946a9 9 0 0 1 2.828-6.471z" fill="#1f1917">
-           </path></g>
-           </svg>
+        <LogoOblix :color="color='#1f1917'" />
     </div>
   <div class="filter_wrap">
-      <!-- <h3>à la carte menu</h3> -->
       <h3>{{currentMenuName}}</h3>
       <div class="filter_title">
         <h6 class="filter_title_h">
@@ -65,8 +43,13 @@
 </template>
 
 <script>
+import LogoOblix from './LogoOblix.vue'
+
 export default {
     name: 'FiltersDishes',
+    components: {
+        LogoOblix,
+    },
     props: {
         useVegeterian: Function,
         useNuts: Function,
